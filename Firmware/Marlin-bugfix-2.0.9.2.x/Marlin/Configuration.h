dfc685a90ed926de8cd5b5d0c52b95c9972ae79c
Jerrett Davis <mxjerrett@gmail.com>
2022-03-17 09:27:16 -0500

    Made probing much faster

--- Firmware/Marlin-bugfix-2.0.9.2.x/Marlin/Configuration.h
+++ Firmware/Marlin-bugfix-2.0.9.2.x/Marlin/Configuration.h
@@ -1187,7 +1187,7 @@
 #define XY_PROBE_FEEDRATE (215*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_FEEDRATE_FAST (4*60)
+#define Z_PROBE_FEEDRATE_FAST (20*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
 #define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2)
@@ -1752,7 +1752,7 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_MM_M { (20*60), (20*60), (6*60) }
+#define HOMING_FEEDRATE_MM_M { (40*60), (40*60), (12*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
